bd918b142d00d5ce56ef1db7f5caa93717200400
KoenR3 <lijst@twilights.be>
2016-06-23 12:55:58 +0200

    Updated data source rest call

--- grafana/setup_grafana.sh
+++ grafana/setup_grafana.sh
@@ -11,7 +11,7 @@ echo "=> Starting Grafana ..."
 sleep 5
 
 echo "Adding datasource ${GRAFANA_DATASOURCE_NAME}"
-curl "http://${GRAFANA_USER}:${GRAFANA_PW}@localhost:9000/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"${GRAFANA_DATASOURCE_NAME}","type":"influxdb","url":"http://localhost:8086","access":"proxy","isDefault":true,"database":"${INFLUXDB_GRAFANA_DB}","user":"${INFLUXDB_GRAFANA_USER}","password":"${INFLUXDB_GRAFANA_PW}"}'
+curl "http://${GRAFANA_USER}:${GRAFANA_PW}@localhost:9000/api/datasources" -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary "{\"name\":\"${GRAFANA_DATASOURCE_NAME}\",\"type\":\"influxdb\",\"url\":\"http://localhost:8086\",\"access\":\"proxy\",\"isDefault\":true,\"database\":\"${INFLUXDB_GRAFANA_DB}\",\"user\":\"${INFLUXDB_GRAFANA_USER}\",\"password\":\"${INFLUXDB_GRAFANA_PW}\"}"
 
 sleep 1
 
